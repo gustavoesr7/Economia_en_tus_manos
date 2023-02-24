418970ff4fdc2fd5526b45955ec8e6fce375e026
gustavoesr7 <110007254+gustavoesr7@users.noreply.github.com>
2023-02-24 09:43:44 -0400

    Versión 1.2.1
    no hemos logrado que los articulos se suban a una base de datos, si lo logramos con los usarios aunque esta fallando

--- Blog/Blog.py
+++ Blog/Blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for,flash
+from flask import Flask, render_template, request, redirect, url_for,flash,session
 from flask_mysqldb import MySQL
 import bcrypt
 
@@ -43,13 +43,75 @@ publicaciones = [
 def home():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        # Recuperar la información del formulario
+        email = request.form['email']
+        password = request.form['password'].encode('utf-8')
+
+        # Crear un cursor y ejecutar la consulta para recuperar la información del usuario
+        cursor = mysql.connection.cursor()
+        query = "SELECT * FROM usuarios WHERE correo = %s"
+        cursor.execute(query, (email,))
+        user = cursor.fetchone()
+        cursor.close()
+
+        # Verificar la contraseña
+        if user and bcrypt.checkpw(password, user['contrasena'].encode('utf-8')):
+            # Iniciar la sesión del usuario
+            session['loggedin'] = True
+            session['id'] = user['id']
+            session['name'] = user['nombre']
+            session['email'] = user['correo']
+
+            # Redirigir al usuario a la página de inicio
+            return redirect(url_for('index'))
+        else:
+            # Mostrar un mensaje de error si las credenciales son incorrectas
+            flash('Credenciales incorrectas.')
+
     return render_template('login.html')
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    return render_template('register.html')
+# Página de registro
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        # Recuperar la información del formulario
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password'].encode('utf-8')
+
+        # Encriptar la contraseña
+        hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+        # Crear un cursor y ejecutar la consulta de inserción en la base de datos
+        cursor = mysql.connection.cursor()
+        query = "INSERT INTO usuarios (nombre, correo, contrasena) VALUES (%s, %s, %s)"
+        cursor.execute(query, (name, email, hashed_password))
+        mysql.connection.commit()
+
+        # Cerrar el cursor y mostrar un mensaje de éxito
+        cursor.close()
+        flash('¡Registro exitoso!')
+
+        # Redirigir al usuario a la página de inicio de sesión
+        return redirect(url_for('login'))
+
+    return render_template('registro.html')
+
+# Cerrar la sesión del usuario
+@app.route('/logout')
+def logout():
+    # Eliminar todas las variables de sesión
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('name', None)
+    session.pop('email', None)
+
+    # Redirigir al usuario a la página de inicio
+    return render_template('/home.html')
+
 
 # Definir la ruta para la página principal
 @app.route('/index')
@@ -64,13 +126,26 @@ def publicacion(id):
 
 @app.route('/admin/upload_article', methods=['GET', 'POST'])
 def upload_article():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-        image = request.files['image']
+    # Verificar si el usuario está logueado
+    if 'loggedin' in session:
+        if request.method == 'POST':
+            titulo = request.form['titulo']
+            contenido = request.form['contenido']
+            imagen = request.files['imagen']
+            fecha_publicacion = request.form['fecha_publicacion']
 
-        flash('Artículo subido con éxito', 'success')
-        return redirect(url_for('index'))
+           # Guardar la imagen en el servidor
+            imagen.save('static/img/' + imagen.filename)
+
+            # Crear un cursor y ejecutar la consulta para insertar el artículo en la base de datos
+            cursor = mysql.connection.cursor()
+            query = "INSERT INTO articulos (titulo, contenido, imagen, autor) VALUES (%s, %s, %s, %s)"
+            cursor.execute(query, (titulo, contenido, imagen.filename, session['nombre']))
+            mysql.connection.commit
+
+            flash('Artículo subido con éxito', 'success')
+            return redirect(url_for('/admin/upload_article'))
+
 
     return render_template('subir-articulo.html')
 
